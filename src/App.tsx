002a2850a4d7d2327458ecace4e116c93352bb0f
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-02-17 23:58:26 +0700

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
