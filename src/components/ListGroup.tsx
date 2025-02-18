b55cf83e4a210be822174a517733e763e0ea4cda
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-02-18 23:51:14 +0700

    Handling Event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,40 +2,52 @@ import React from 'react'
 
 const ListGroup = () => {
   const items=[
-    'New York',
-    'San Francisco',
-    'Tokyo',
-    'London',
-    'Paris'
+    { "rank": 1, "name": "Alice", "score": 2500 },
+    { "rank": 2, "name": "Bob", "score": 2300 },
+    { "rank": 3, "name": "Charlie", "score": 2100 },
+    { "rank": 4, "name": "Diana", "score": 1900 },
+    { "rank": 5, "name": "Eve", "score": 1700 },
   ]
  
   
   const getMessage= ()=>{
     return items.length===0 && <p>No item found </p> 
   }
-//    const items=[]
-//   if(items.length===0)
-//     return (
-//     <>
-//         <h1>Top Scores</h1>
-//         <p>No item found</p>
-//     </>
-    
-//     )
 
+  const handleClick=(e:MouseEvent)=>{
+     console.log(e);
+     
+  }
    
-
   return (
     <>
       <h1>Top Scores</h1>
-       {getMessage()}
-      <ul className="list-group" >
-        {items.map((item)=>(
-            <li className='list-group-item' key={item}>{item}</li>
-        ))}
-     </ul>
-       
-  
+{getMessage()}
+<table className="table">
+  <thead>
+    <tr>
+      <th scope="col">Rank</th>
+      <th scope="col">Name</th>
+      <th scope="col">Score</th>
+    </tr>
+  </thead>
+  <tbody>
+        {
+            items.map((item)=>(
+                <tr className=''
+                 onClick={handleClick}
+                 key={item.rank}>
+                <td>{item.rank}</td>
+                <td>{item.name}</td>
+                <td>{item.score}</td>
+                </tr>
+            ))
+        }
+
+
+  </tbody>
+</table>
+    
    
     </>
   )
