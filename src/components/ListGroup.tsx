3d4812546fd774f8b90c5fbb1ce98d7ae34e051d
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-02-18 22:54:38 +0700

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -8,11 +8,27 @@ const ListGroup = () => {
     'London',
     'Paris'
   ]
+ 
+  
+  const getMessage= ()=>{
+    return items.length===0 && <p>No item found </p> 
+  }
+//    const items=[]
+//   if(items.length===0)
+//     return (
+//     <>
+//         <h1>Top Scores</h1>
+//         <p>No item found</p>
+//     </>
+    
+//     )
+
    
 
   return (
     <>
       <h1>Top Scores</h1>
+       {getMessage()}
       <ul className="list-group" >
         {items.map((item)=>(
             <li className='list-group-item' key={item}>{item}</li>
